5291f888b337b565b6d9354dc3a69ecdc1b96feb
aparkening <aaron.parkening@gmail.com>
2019-10-24 16:16:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+// For loop. writeCards(array of string names, event name) that users for to loop
+function writeCards(names, eventName) {
+  let thankYouArray = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouArray;
+}
+
+// while countDown
+function countdown(amount) {
+  let i = amount;
+  while (i >= 0) {
+    console.log(i)
+    i--;
+  }
+}
