15449c01862d75f50f22bbf0f5cfa0b5db79eae9
Renee Nordholm <reneenordholm@gmail.com>
2019-10-26 19:43:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+function writeCards(names, special) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${special} gift!`);
+    }
+    return array;
+}
