29887ee63c9b170ae2b43d0b848852510b25333c
Garth Lilly <garff247@gmail.com>
2019-11-08 19:28:23 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return cards;
+}
+
+function countdown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
