68e1f44907240d83f7d77ab792086253dc1de1ac
Meg Galvez <meg@oakmaine.com>
2019-11-10 10:20:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const thankYou = [ 'Lisa', 'Kaitlin', 'Jan' ]
+const holiday = "surprise"
+
+function writeCards(thankYou, holiday) {
+    let array = []
+    for (let i = 0; i < thankYou.length; i++){
+        array.push(`Thank you, ${thankYou[i]}, for the wonderful ${holiday} gift!`);
+           
+    }
+    return array
+}   
+
+
+function countdown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
