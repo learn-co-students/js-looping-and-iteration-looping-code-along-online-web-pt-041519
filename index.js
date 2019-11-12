6dba87ecf4c9a75d65f8670612014f7d1e0691dc
earthctzn <earthctzn1@gmail.com>
2019-11-11 23:02:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let myarray = []
+  for (let i = 0; i < names.length; i++) {
+   myarray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`) 
+  }
+  return myarray 
+}
+
+function countdown(num) {
+  let i = num
+  while (num >= 0){
+    console.log(num--)
+  }
+}
