95c9be3a9e5f9eb6c42cb1d853ea8327449fd1cb
Kelsey Low <kklowlow@gmail.com>
2019-11-13 03:00:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  const cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return cards
+}
+
+function countdown(int) {
+  while (int >= 0) {
+    console.log(int)
+    int--
+  }
+}
